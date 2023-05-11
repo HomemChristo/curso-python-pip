a69b3bec6353211129f10f8fb85f853bd3168f8d
HomemChristo <jhn.patino@gmail.com>
2023-05-11 16:43:34 -0500

    Graficas paises

---
+++ app/read_csv.py
@@ -0,0 +1,31 @@
+import csv
+
+def read_csv(path):
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)
+        data = []
+        for row in reader:
+            iterable = zip(header, row)
+            country_dict = {key: value for key, value in iterable}
+            data.append(country_dict)
+        return data
+
+if __name__ == '__main__':
+    data = read_csv('./app/data.csv')
+    print(data[0])
+
+
+
+###########################
+#EJERCICIO
+
+""" 
+
+def read_csv(path):
+   with open(path, 'r') as csvfile:      
+      total = sum(int(r[1]) for r in csv.reader(csvfile))
+      return total
+
+response = read_csv('./data.csv')
+print(response) """
